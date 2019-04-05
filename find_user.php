d4937764054ceef423af703cc013ca4677df0cd9
datagutten <datagutten@datagutten.net>
2019-04-05 13:28:09 +0200

    Change to exception based error handling

--- find_user.php
+++ find_user.php
@@ -1,28 +1,37 @@
 <?Php
 session_start();
+if(empty($_SESSION['reset']))
+    die(json_encode(array('error'=>'Not logged in')));
 require 'adtools/adtools.class.php';
-$adtools=new adtools;
-require 'config.php';
 
-$result=$adtools->connect_and_bind($config['dc'],$_SESSION[$config['dc']]['username'].'@'.$config['upn_suffix'],$_SESSION[$config['dc']]['password'],true);
-
-if($result===false)
-	echo json_encode(array('error'=>$adtools->error));
-else
+try
 {
-	if(!isset($_GET['username']) && isset($argv[1]))
-		$_GET['username']=$argv[1];
+    $ad_tools=new adtools('reset');
+    $ad_tools->connect_and_bind(null,$_SESSION['reset']['username'].'@'.$ad_tools->config['domain'],$_SESSION['reset']['password']);
+    if(!isset($_GET['username']) && isset($argv[1]))
+        $_GET['username']=$argv[1];
 
-	$mobile=$adtools->find_object($_GET['username'],$config['base_dn'],'username',array('mobile','displayName','pwdLastSet'));
-	if(!isset($config['sms']))
-		$mobile['mobile'][0]=false;
-	elseif(!isset($mobile['mobile']))
-		$mobile['mobile'][0]='';
-	if(!empty($adtools->error))
-		echo json_encode(array('error'=>$adtools->error));
-	else
-	{
-		$pwdlastset=date('Y-m-d H:i',$adtools->microsoft_timestamp_to_unix($mobile['pwdlastset'][0]));
-		echo json_encode(array('mobile'=>$mobile['mobile'][0],'displayName'=>$mobile['displayname'][0],'dn'=>$mobile['dn'],'error'=>'','pwdlastset'=>$pwdlastset));
-	}
+    $user=$ad_tools->find_object($_GET['username'],false,'username',array('mobile','displayName','pwdLastSet'));
+
+    if(!empty($user))
+    {
+        if(!isset($user['mobile']))
+            $user['mobile'][0]='';
+        $pwdlastset_timestamp=$ad_tools->microsoft_timestamp_to_unix($user['pwdlastset'][0]);
+        $pwdlastset=date('Y-m-d H:i',$pwdlastset_timestamp);
+        $diff=time()-$pwdlastset_timestamp;
+        $diff_days=$diff/(3600*24);
+        echo json_encode(array(
+            'mobile'=>$user['mobile'][0],
+            'displayName'=>$user['displayname'][0],
+            'dn'=>$user['dn'],'error'=>'',
+            'pwdlastset'=>$pwdlastset,
+            'diff_days'=>(int)$diff_days));
+    }
+    else
+        echo json_encode(array('error'=>' '));
+}
+catch (Exception $e)
+{
+    echo json_encode(array('error'=>$e->getMessage()));
 }
